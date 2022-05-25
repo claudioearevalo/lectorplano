dbbe382f0511c8a19958aad9d6a31567936b8cc1
claudioearevalo <77679642+claudioearevalo@users.noreply.github.com>
2022-05-25 09:44:23 -0300

    Initial Commit

---
+++ lector/lectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  lectorApp.swift
+//  lector
+//
+//  Created by Claudio Arevalo on 25/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct lectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
